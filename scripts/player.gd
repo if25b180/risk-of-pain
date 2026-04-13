008c1a40e4dcce6f1b5c263faf6bcabdd040cc2b
Johannes Leiter <johannes@leiter.cc>
2026-04-13 20:31:40 +0200

    feat: basic player controller & sprites

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,3 +1,35 @@
 extends CharacterBody2D
 
-# TODO: implement
+@onready var animation = $AnimationPlayer
+@onready var sprite = $Sprite2D
+
+@export var speed := 200.0
+@export var gravity := 900.0
+@export var jump_force := -300.0
+
+func _physics_process(delta):
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
+
+	
+	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	velocity.x = direction * speed
+
+	
+	move_and_slide()
+	if direction != 0:
+		sprite.flip_h = direction < 0
+
+
+  
+	if direction != 0:
+		if animation.current_animation != "p_run":
+			animation.play("p_run")
+	else:
+		if animation.current_animation != "p_idle":
+			animation.play("p_idle")
