7a663ad9b6d506e6923d9daa6660f9ad40586cf6
Johannes Leiter <johannes@leiter.cc>
2026-04-21 15:37:07 +0200

    feat: added vorzeigbares

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,11 +2,16 @@ extends CharacterBody2D
 
 @onready var animation = $AnimationPlayer
 @onready var sprite = $Sprite2D
+@onready var jump_sfx: AudioStreamPlayer2D = $jump_SFX
+@onready var walk_sfx: AudioStreamPlayer2D = $walk_SFX
 
 
 @export var speed := 200.0
 @export var gravity := 900.0
 @export var jump_force := -300.0
+
+var was_on_floor := false
+
 
 func _physics_process(delta):
 	if not is_on_floor():
@@ -14,6 +19,8 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_force
+		animation.play("p_jump")
+		jump_sfx.play()
 	
 	var direction = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	velocity.x = direction * speed
@@ -23,8 +30,15 @@ func _physics_process(delta):
 	if direction != 0:
 		sprite.flip_h = direction < 0
 		
-		if animation.current_animation != "p_run":
-			animation.play("p_run")
+	if not is_on_floor():
+		if animation.current_animation != "p_air":
+			animation.play("p_air")
 	else:
-		if animation.current_animation != "p_idle":
-			animation.play("p_idle")
+		if direction != 0:
+			if animation.current_animation != "p_run":
+				animation.play("p_run")
+				if is_on_floor():
+					walk_sfx.play()
+		else:
+			if animation.current_animation != "p_idle":
+				animation.play("p_idle")
